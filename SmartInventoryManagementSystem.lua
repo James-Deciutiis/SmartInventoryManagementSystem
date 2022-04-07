c56a79175b46adc3dd55dddaac02a81c42ae3143
James-Deciutiis <jamesdeciutiis@gmail.com>
2022-04-06 19:07:31 -0700

    feat: added table to story flags, queries for ilvl now successfully filter inventory

--- SmartInventoryManagementSystem.lua
+++ SmartInventoryManagementSystem.lua
@@ -1,13 +1,15 @@
 SLASH_SIMS1 = "/sims"
 
-local iLvlFlag = false
+flags = { }
+flags["Item Level"] = false
+flags["Equipment"] = false
 
-function CreateCheckButton(name, parent, text, position, x, y, flag)
+function CreateCheckButton(name, parent, text, position, x, y)
   local CheckButton = CreateFrame("CheckButton", name, parent, "ChatConfigCheckButtonTemplate")
   CheckButton:SetPoint(position, x, y)
   getglobal(CheckButton:GetName() .. "Text"):SetText(text)
   CheckButton:SetScript("OnClick", function()
-    flag = not flag
+    flags[text] = not flags[text]
   end)
   return CheckButton
 end
@@ -28,7 +30,7 @@ function MainFrame_Show()
 	if not MainFrame then
     local f = CreateFrame("Frame", "MainFrame", UIParent, "BasicFrameTemplateWithInset")
 		f:SetPoint("CENTER")
-		f:SetSize(600, 500)
+		f:SetSize(300, 500)
 		f:SetMovable(true)
 		f:SetClampedToScreen(true)
 		f:SetScript("OnMouseDown", function(self, button)
@@ -42,20 +44,27 @@ function MainFrame_Show()
     f.title:SetPoint("CENTER", f.TitleBg, 5, 0)
     f.title:SetText("S.I.M.S")
 
-    local iLvlButton = CreateCheckButton("ItemLevelCheckBox", MainFrame, "Item Level", "CENTER", -110, 0, iLvlFlag)
-    local iLvlEditBox = CreateEditBox("ItemLevelEditBox", MainFrame, "CENTER", 30, 0)
+    local iLvlButton = CreateCheckButton("ItemLevelCheckBox", MainFrame, "Item Level", "TOP", -75, -40)
+    local iLvlEditBox = CreateEditBox("ItemLevelEditBox", MainFrame, "TOP", 65, -43)
     
+    local equipmentButton = CreateCheckButton("EquipmentCheckBox", MainFrame, "Equipment", "TOP", -75, 50)
+    local equipmentEditBox = CreateEditBox("EquipmentEditBox", MainFrame, "TOP", 65, -53)
 
     local button = CreateFrame("Button", "AcceptButton", MainFrame, "GameMenuButtonTemplate")
     button:SetPoint("BOTTOM", 0, 10)
     button:SetText("Okay")
     button:SetScript("OnClick", function(self)
-      local query = editBox:GetText()
-      for currentBag = BACKPACK_CONTAINER, NUM_BAG_SLOTS do 
+      local query = iLvlEditBox:GetText()
+      for currentBag = BACKPACK_CONTAINER, NUM_BAG_SLOTS do
         for slot = 1, GetContainerNumSlots(currentBag) do
           local itemLink = GetContainerItemLink(currentBag, slot)
           if(itemLink) then
-            print(GetItemInfo(itemLink))
+            itemName, itemL, itemQuality, itemLevel, itemMinLevel, itemType, itemSubType, itemStackCount, itemEquipLoc, itemTexture, sellPrice, classID, subclassID, bindType, expacID, setID, isCraftingReagent = GetItemInfo(itemLink)
+            if(flags["Item Level"] and itemLevel) then
+              if(tonumber(query) == itemLevel) then
+                print(itemLink)
+              end
+            end
           end
         end
       end
